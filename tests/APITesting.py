5363b183d59ceeb18452953cab7c018fa57ae20b
sergey <sergiusperlov@gmail.com>
2025-11-17 00:18:01 +0200

    change in comments

--- tests/APITesting.py
+++ tests/APITesting.py
@@ -4,12 +4,14 @@ import requests
 BASE_URL = "https://jsonplaceholder.typicode.com"
 
 def test_get_post():
-    response = requests.get(f"{BASE_URL}/posts/1")
+    response = requests.get(f"{BASE_URL}/comments/1")
     assert response.status_code == 200
     data = response.json()
     assert data["id"] == 1
-    assert "title" in data
-    assert "body" in data
+    assert "name" in data
+    assert "email" in data
+    print(data["body"])
+    print(data["email"])
 
 def test_create_post():
     payload = {
